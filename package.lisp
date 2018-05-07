80758cb16fff751336f9b3f63bed6d1615da170d
Thomas de Grivel <thoxdg@gmail.com>
2018-05-07 12:04:54 +0200

    update license

--- package.lisp
+++ package.lisp
@@ -1,4 +1,5 @@
-;; cl-bitcoin copyright Thomas de Grivel <thoxdg@gmail.com> 0614550127
+;; cl-bitcoin - Peer to peer money
+;; Copyright 2018 Thomas de Grivel <thoxdg@gmail.com> 0614550127
 
 (in-package :common-lisp)
 
